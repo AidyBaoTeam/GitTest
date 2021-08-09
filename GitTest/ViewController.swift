b133e5d06d880e179ce820b666165add780fcd92
AidyBao <281907061@qq.com>
2021-08-09 11:05:33 +0800

    first commit

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let lab = UILabel(frame: CGRect(x: 0, y: 100, width: 200, height: 50))
+        view.addSubview(lab)
     }
 
 
